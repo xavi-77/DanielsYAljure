ac545ec3f878961037d961db183cb9136c8e07cf
xavi-77 <67208829+xavi-77@users.noreply.github.com>
2021-06-05 14:43:20 -0500

    Update bd
    Actualize importacion de la base de datos

--- backend/model/persona.js
+++ backend/model/persona.js
@@ -1,6 +1,6 @@
 'use strict'
 const Sequelize = require('sequelize');
-const db = require('../db/database');
+const db = require('../database/db');
 
 module.exports = db.Sequelize.define(
     'personas',{
